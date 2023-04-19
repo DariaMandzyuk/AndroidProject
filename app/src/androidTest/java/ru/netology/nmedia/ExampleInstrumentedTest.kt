95950c52621701ab3646d5b94f9c3efc1f1e518c
Daria Mandziuk <dariacolorso@gmail.com>
2023-04-19 18:17:50 +0400

    change folders

--- app/src/androidTest/java/ru/netology/nmedia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/netology/nmedia/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication2
+package com.example.nmedia
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication2
+package com.example.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
